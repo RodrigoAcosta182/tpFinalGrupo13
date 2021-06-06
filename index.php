e7c4b506ff9c2e007f8d3401f4bb96405b8824ba
julioRodrigoAcosta <jracosta1991@gmail.com>
2021-06-05 21:23:45 -0300

    Se agrego Router/htacces/UrlHelper

--- index.php
+++ index.php
@@ -1,19 +1,35 @@
 <?php
 //configuracion del proyecto
 include_once("helpers/Configuracion.php");
+
 session_start();
+$configuration = new Configuracion();
 
-$page = isset( $_GET['page'])? $_GET['page'] : "login" ;
+$urlHelper = $configuration->getUrlHelper();
+$module = $urlHelper->getModuleFromRequestOr("login");
+$action = $urlHelper->getActionFromRequestOr("execute");
 
-switch ($page){
-    case "login":
-        $loginController = Configuracion::getLoginController();
-        $loginController->renderLogin();
-        break;
-    case "inicio":
-        $inicioController = Configuracion::getInicioController();
-        $inicioController->renderInicio();
-        break;
-}
+
+$router = $configuration->getRouter();
+$router->executeActionFromModule($action, $module);
+
+
+
+
+//$page = isset( $_GET['page'])? $_GET['page'] : "login" ;
+//
+//switch ($page){
+//    case "login":
+//        $loginController = Configuracion::getLoginController();
+//        $loginController->renderLogin();
+//        break;
+//    case "inicio":
+//        $inicioController = Configuracion::getInicioController();
+//        $inicioController->renderInicio();
+//        break;
+//}
+
+
+
 
 
