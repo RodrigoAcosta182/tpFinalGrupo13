976c4e840f5d169fcdfd2cc25c0a28983f41333a
Emiliano-Ortiz <emiortiz1992@gmail.com>
2021-06-30 15:18:31 -0300

    Generar proforma en PDF

--- controller/ProformaController.php
+++ controller/ProformaController.php
@@ -17,52 +17,89 @@ class ProformaController
 
         if (isset($_SESSION["logueado"])) {
 
-
+            echo $this->render->renderizar("view/proforma.mustache");
         } else {
             header("location: /tpFinalGrupo13");
             exit();
         }
     }
 
-    public function generarProforma(){
-        require('third-party/fpdf/fpdf.php');
-        $idViaje = $_POST['idProforma'];
-        $arrayProforma [] = $this->proformaModel->getProformaById($idViaje);
-
-        $jsonProforma = json_encode($arrayProforma);
-
-        json_decode($jsonProforma);
-
-//        $newClient = array(
-//            $idViajardo =>"email"
-//        );
-
-//        $pdf = new FPDF();
-//        $pdf->AddPage();
-//        $pdf->AliasNbPages();
-//
-//        //$pdf->Image($qr,161, 0, 50, 0, "png");
-//
-//        $pdf->SetFont('Arial', '', 16);
-//        $pdf->Cell(50, 10, "Proforma Garlopa Company", 0, 1 );
-//
-//        $pdf->Cell(150, 10, utf8_decode("N° de Viaje: $idViaje"), 1, 1, 'C', 0);
-//        $pdf->Cell(50, 10, "Fecha", 1);
-//        $pdf->Cell(100, 10, date('d/m/Y'),1,1,'C');
-//        $pdf->Cell(50, 10, "", 0, 1);
-//        $pdf->Cell(50, 10, "Gastos", 0, 1);
-//        $pdf->Cell(50, 10, "Viaticos", 1, 0);
-//        $pdf->Cell(100, 10, "$viaticos", 1, 1, 'C', 0);
-//        $pdf->Cell(50, 10, utf8_decode("Gastos de Peaje"), 1, 0);
-//        $pdf->Cell(100, 10, "$peaje", 1, 1, 'C', 0);
-//        $pdf->Cell(50, 10, utf8_decode("Gastos extra"), 1, 0);
-//        $pdf->Cell(100, 10, "$extra", 1, 1, 'C', 0);
-//        $pdf->Cell(50, 10, "Email", 1, 0);
-//        $pdf->Cell(100, 10, "$extra", 1, 1, 'C', 0);
-//
-//
-//        $pdf->Output("", "Proforma  - ID Viaje $idViaje");
+    public function consultarProforma()
+    {
+        $data = array();
+        if (isset($_SESSION["logueado"])) {
+            $idProforma = $_POST['idProforma'];
+            $data["proforma"]= $this->proformaModel->getProformaById($idProforma);
+            echo $this->render->renderizar("view/proforma.mustache", $data);
+        }
+        else {
+            header("location: /tpFinalGrupo13");
+            exit();
+        }
     }
 
+    public function generarProforma(){
+        $data = array();
+        if (isset($_SESSION["logueado"])) {
+            if (isset($_POST['usuario']) && isset($_POST['sucuOrig']) && isset($_POST['sucuDest']) &&
+                isset($_POST['cliente']) && isset($_POST['vehiculo']) && isset($_POST['arrastre']) &&
+                isset($_POST['fechaOrig']) && isset($_POST['fechaEst']) && isset($_POST['kmEst']) &&
+                isset($_POST['combEst']) && isset($_POST['precio']) && isset($_POST['otrosG'])){
+                $idProforma =  $_POST['viajeId'];
+                $usuario = $_POST['usuario'];
+                $sucuOrig = $_POST['sucuOrig'];
+                $sucuDest = $_POST['sucuDest'];
+                $cliente = $_POST['cliente'];
+                $vehiculo = $_POST['vehiculo'];
+                $arrastre = $_POST['arrastre'];
+                $fechaOrig = $_POST['fechaOrig'];
+                $fechaEst = $_POST['fechaEst'];
+                $kmEst = $_POST['kmEst'];
+                $combEst = $_POST['combEst'];
+                $precio = $_POST['precio'];
+                $otrosG = $_POST['otrosG'];
+            }
 
+
+        }
+        else {
+            header("location: /tpFinalGrupo13");
+            exit();
+        }
+
+        require('third-party/fpdf/fpdf.php');
+
+        $pdf = new FPDF();
+        $pdf->AddPage();
+        $pdf->AliasNbPages();
+
+
+
+        //$pdf->Image($qr,161, 0, 50, 0, "png");
+
+        $pdf->SetFont('Arial', '', 16);
+        $pdf->Cell(50, 10, "Proforma Garlopa Company", 0, 1 );
+
+        $pdf->Cell(150, 10, utf8_decode("N° de Viaje: $idProforma"), 1, 1, 'C', 0);
+        $pdf->Cell(50, 10, "Fecha Salida", 1);
+
+        $pdf->Cell(100, 10, date('d/m/Y'),1,1,'C');
+        $pdf->Cell(50, 10, "", 0, 1);
+        $pdf->Cell(50, 10, "Datos", 0, 1);
+        $pdf->Cell(50, 10, "Chofer", 1, 1);
+        $pdf->Cell(100, 10, "$usuario", 1, 1, 'C', 0);
+
+        $pdf->Cell(50, 10, "Gastos", 0, 1);
+        $pdf->Cell(50, 10, "Otros Gastos", 1, 0);
+        $pdf->Cell(100, 10, "$otrosG", 1, 1, 'C', 0);
+//        $pdf->Cell(50, 10, utf8_decode("Gastos de Peaje"), 1, 0);
+//        $pdf->Cell(100, 10, "$cliente", 1, 1, 'C', 0);
+//        $pdf->Cell(50, 10, utf8_decode("Gastos extra"), 1, 0);
+//        $pdf->Cell(100, 10, "$sucuOrig", 1, 1, 'C', 0);
+//        $pdf->Cell(50, 10, "Email", 1, 0);
+//        $pdf->Cell(100, 10, "$precio", 1, 1, 'C', 0);
+
+
+        $pdf->Output("", "Proforma  - ID Viaje $idProforma");
+   }
 }
--- model/ProformaModel.php
+++ model/ProformaModel.php
@@ -11,21 +11,7 @@ class ProformaModel{
 
     public function getProformaById($idViaje)
     {
-        return $this->database->consulta("select viajeId as ViajeId,
-                                                    fechaOrigen as FechaOrigen,
-                                                    fechaEstimada as FechaEstimada,
-                                                    precioViaje as Precio,
-                                                    KmEstimado as Km,
-                                                    CombustibleEst as Combustible,
-                                                    OtrosGastos as Og,
-                                                    usuario as Chofer,
-                                                    cliente as Cliente,
-                                                    patenteVehiculo as Vehiculo,
-                                                    patenteArrastre as Arrastre,
-                                                    SucursalOrigen as SucOrigen,
-                                                    SucursalDestino as SucDestino
-                                              from vproforma
-                                              where viajeId = '$idViaje'");
+        return $this->database->consulta("select * from vviajes where viajeId = '$idViaje'");
     }
 
 
