845aac8ffc37eb7ff796128768f7977c7fdb5cd0
julioRodrigoAcosta <jracosta1991@gmail.com>
2021-07-03 00:37:12 -0300

    Formulario posicion de GoogleMaps

--- controller/PosicionController.php
+++ controller/PosicionController.php
@@ -13,12 +13,31 @@ class PosicionController
 
     public function execute()
     {
+        $data = array();
         if (isset($_SESSION["logueado"])) {
-        $posicion["posicion"] = $this->posicionModel->listarPosicion();
-        echo $this->render->renderizar("view/posicionvehiculo.mustache", $posicion);
+        //$data["posicion"] = $this->posicionModel->listarPosicion();
+        echo $this->render->renderizar("view/posicionvehiculo.mustache", $data);
         } else {
             header("location: /tpFinalGrupo13");
             exit();
         }
+    }
+
+    public function obtenerPosicion(){
+        $datos = array();
+        $datos['latitud'] = $_POST['latitud'];
+        $datos['longitud'] = $_POST['longitud'];
+
+        if(!empty($datos)) {
+            echo json_encode($datos);
+        } else {
+            echo json_encode(['campo1'=>'', 'campo2'=>'']);
+        }
+
+
+    }
+
+    public function registrarPosicion(){
+
     }
 }
